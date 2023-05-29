f92e0746a347d3a81efb8ddb6055cbbb74eab968
Kurt0120 <134949330+Kurt0120@users.noreply.github.com>
2023-05-30 00:18:32 +0800

    Add files via upload

---
+++ dataupload/dataupload/Form1.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Data
+Imports System.IO
+Imports Microsoft.VisualBasic.FileIO
+Imports OfficeOpenXml
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles UploadButton_Click.Click
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "CSV files (*.csv)|*.csv"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim filePath As String = openFileDialog.FileName
+
+            ' Load data from CSV file
+            Dim dataTable As DataTable = LoadDataFromCSV(filePath)
+
+            ' Display the data in the DataGridView
+            DataGridView1.DataSource = dataTable
+        End If
+    End Sub
+    Private Function LoadDataFromCSV(filePath As String) As DataTable
+        Dim dataTable As New DataTable()
+
+        Using parser As New TextFieldParser(filePath)
+            parser.TextFieldType = FieldType.Delimited
+            parser.SetDelimiters(",")
+
+            ' Read the header row
+            Dim headers As String() = parser.ReadFields()
+            For Each header In headers
+                dataTable.Columns.Add(header)
+            Next
+
+            ' Read the data rows
+            While Not parser.EndOfData
+                Dim fields As String() = parser.ReadFields()
+                dataTable.Rows.Add(fields)
+            End While
+        End Using
+
+        Return dataTable
+    End Function
+
+    Private Sub PrintButton_Click(sender As Object, e As EventArgs) Handles PrintButton.Click
+        Utilities.ExportToExcel(DataGridView1)
+    End Sub
+
+End Class
---
+++ dataupload/dataupload/Utilities.vb
@@ -0,0 +1,35 @@
+﻿Imports System.IO
+Imports OfficeOpenXml
+
+Module Utilities
+    Public Sub ExportToExcel(dataGrid As DataGridView)
+        ' Set the LicenseContext to properly handle licensing
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial
+
+        Using package As New ExcelPackage()
+            Dim worksheet = package.Workbook.Worksheets.Add("Sheet1")
+
+            ' Write column headers
+            For i As Integer = 0 To dataGrid.Columns.Count - 1
+                worksheet.Cells(1, i + 1).Value = dataGrid.Columns(i).HeaderText
+            Next
+
+            ' Write data rows
+            For i As Integer = 0 To dataGrid.Rows.Count - 1
+                For j As Integer = 0 To dataGrid.Columns.Count - 1
+                    Dim cellValue = If(dataGrid.Rows(i).Cells(j).Value IsNot Nothing, dataGrid.Rows(i).Cells(j).Value.ToString(), "")
+                    worksheet.Cells(i + 2, j + 1).Value = cellValue
+                Next
+            Next
+
+            ' Save the Excel file
+            Dim saveFileDialog As New SaveFileDialog()
+            saveFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx"
+            If saveFileDialog.ShowDialog() = DialogResult.OK Then
+                Dim filePath As String = saveFileDialog.FileName
+                Dim fileInfo As New FileInfo(filePath)
+                package.SaveAs(fileInfo)
+            End If
+        End Using
+    End Sub
+End Module
